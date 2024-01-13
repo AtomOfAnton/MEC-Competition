8041730b389a6ca58858318bbcb6594e97cf1d19
Anthony Golubev <AnthonyVGolubev@gmail.com>
2024-01-13 12:53:47 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -4,5 +4,18 @@
 
 import math
 import pandas as pd
-datasheet = pd.read_excel(r"C:\Users\Antho\Downloads\Dataset.xlsx")
-print(datasheet)
+df = pd.read_excel(r"C:\Users\Antho\Downloads\Dataset.xlsx")
+datasheetdict = {}
+index = df.iloc[:,1]
+title = df.iloc[:,2]
+description = df.iloc[:,3]
+max_salary = df.iloc[:,4]
+med_salary = df.iloc[:,5]
+min_salary = df.iloc[:,6]
+pay_period = df.iloc[:,7]
+location = df.iloc[:,8]
+experience_level = df.iloc[:,9]
+skills = df.iloc[:,10]
+accessibiliy_features = df.iloc[:,11]
+
+print(index,title,description,max_salary,med_salary,min_salary,pay_period,location,experience_level,skills,accessibiliy_features)
