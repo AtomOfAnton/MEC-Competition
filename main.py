2a9fd9b49101a4faf6be049414ca6d8c6c7120bc
Anthony Golubev <AnthonyVGolubev@gmail.com>
2024-01-13 14:51:59 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -6,17 +6,24 @@ import math
 import pandas as pd
 df = pd.read_excel(r"C:\Users\Antho\Downloads\Dataset.xlsx")
 datasheetdict = {}
-index = df.iloc[:,1]
-title = df.iloc[:,2]
-description = df.iloc[:,3]
-max_salary = df.iloc[:,4]
-med_salary = df.iloc[:,5]
-min_salary = df.iloc[:,6]
-pay_period = df.iloc[:,7]
-job_type = df.iloc[:,8]
-location = df.iloc[:,9]
-experience_level = df.iloc[:,10]
-skills = df.iloc[:,11]
-accessibility_features = df.iloc[:,12]
+index = df.iloc[:,0]
+title = df.iloc[:,1]
+description = df.iloc[:,2]
+max_salary = df.iloc[:,3]
+med_salary = df.iloc[:,4]
+min_salary = df.iloc[:,5]
+pay_period = df.iloc[:,6]
+job_type = df.iloc[:,7]
+location = df.iloc[:,8]
+experience_level = df.iloc[:,9]
+skills = df.iloc[:,10]
+accessibility_features = df.iloc[:,11]
 
+temp = []
+print(pay_period.values())
+for x in pay_period.values():
+    if x not in temp:
+        temp.append(x)
+print(temp)
 importance_order = (skills,accessibility_features,experience_level,job_type, min_salary)
+attributes = []
