9597de4627ca14e33b0ac2e84728fe3c0b76b026
Anthony Golubev <AnthonyVGolubev@gmail.com>
2024-01-13 11:48:35 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -2,5 +2,7 @@
 #Anthony Golubev, Mihir Patel
 #13/01/2024
 
-print("Hello, World!")
-print("no")
+import math
+import pandas as pd
+datasheet = pd.read_excel(r"C:\Users\Antho\Downloads\Dataset.xlsx")
+print(datasheet)
